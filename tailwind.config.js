ea0760684992b1d12354ffdb6cacdc77db2cad7f
Aman11b <amansingh1100b@gmail.com>
2025-08-28 17:46:38 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        orange:colors.orange,
+        teal: colors.teal,
+        'light-blue': colors.lightBlue,
+      }
+
+    },
+  },
+  plugins: [],
+}
